04f658d3d0d440ba9197e855e7888b2efd184d7f
swap769 <swap769@gmail.com>
2022-12-27 22:58:57 +0530

    new form added

--- MyFirstWeb/Models/Userdata.cs
+++ MyFirstWeb/Models/Userdata.cs
@@ -3,9 +3,10 @@
     public class Userdata
     {
         public string Name { get; set; }
-        public int Age { get; set; }
+        public string Email { get; set; }
+        public int Dob { get; set; }
         public string Designation { get; set;}
         public int Salary { get; set;}
-
+      
     }
 }
