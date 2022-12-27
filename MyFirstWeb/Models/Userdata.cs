8ee568099564471eb0cc228969a816348844a506
swap769 <swap769@gmail.com>
2022-12-27 16:25:06 +0530

    user data class added in model

---
+++ MyFirstWeb/Models/Userdata.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFirstWeb.Models
+{
+    public class Userdata
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Designation { get; set;}
+        public int Salary { get; set;}
+
+    }
+}
