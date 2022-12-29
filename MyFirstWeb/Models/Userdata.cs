e944b361e40fa1aa005e594223a60a7613eb0bc6
swap769 <swap769@gmail.com>
2022-12-29 17:34:30 +0530

    changes added in userdata

--- MyFirstWeb/Models/Userdata.cs
+++ MyFirstWeb/Models/Userdata.cs
@@ -1,16 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using System.Threading.Tasks;
 namespace MyFirstWeb.Models
 {
     public class Userdata
     {
-        [Required(ErrorMessage = "Please enter name")]
+        [Required(ErrorMessage = "Please enter name"), MaxLength(30)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter dob")]
         public DateTime Dob { get; set; }
+
+        [Required(ErrorMessage = "Please enter designation")]
         public string Designation { get; set;}
+
+        [Required(ErrorMessage = "Please enter salary")]
         public int Salary { get; set;}
+
+        [Required(ErrorMessage = "Please enter Gender")]
         public Gender Gender { get; set; }
+
+        [Required(ErrorMessage = "Please enter married")]
         public string Married { get; set;}
     }
     public enum Gender
