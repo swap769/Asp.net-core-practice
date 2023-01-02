b77e4d5cdf64e34c443ea6269d434183f072207f
swap769 <swap769@gmail.com>
2023-01-02 22:41:44 +0530

    employeecontext added

---
+++ MyFirstWeb/Models/EmployeeDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstWeb.Models
+{
+    public class EmployeeDatabase
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Gender { get; set; }
+        public string? Designation { get; set; }
+        public int PhoneNo { get; set; }
+    }
+}
---
+++ MyFirstWeb/Models/EmpoyleeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstWeb.Models
+{
+    public class EmpoyleeContext : DbContext
+    {
+        public EmpoyleeContext(DbContextOptions<EmpoyleeContext>options) : base(options) { 
+
+        }
+        public DbSet<EmployeeDatabase> Employees { get; set; }
+
+    }
+} 
